5bfae064019541fd222f04b3fa531a611f1fa84e
Lincoln Garcia <84988438+lincolngarcia@users.noreply.github.com>
2026-03-10 21:16:38 -0600

    Implement SQL Server

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -1,6 +1,8 @@
 package server;
 
 import chess.*;
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
 
 public class ServerMain {
     public static void main(String[] args) {
@@ -8,5 +10,15 @@ public class ServerMain {
         server.run(8080);
 
         System.out.println("♕ 240 Chess Server");
+
+        // See if the database is active
+
+        // Load in any data from database
+
+        try {
+            DatabaseManager.createDatabase();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
