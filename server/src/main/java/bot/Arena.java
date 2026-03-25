997c1ae007d2f636bdefa57cfdb80e8feaafe0ab
Lincoln Garcia <84988438+lincolngarcia@users.noreply.github.com>
2026-03-25 17:33:15 -0600

    Win Conditions

--- server/src/main/java/bot/Arena.java
+++ server/src/main/java/bot/Arena.java
@@ -7,7 +7,6 @@ import chess.InvalidMoveException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
 
