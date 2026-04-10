adc13fc05be843c4e88c1bb027a33509188a44a6
Lincoln Garcia <84988438+lincolngarcia@users.noreply.github.com>
2026-04-10 00:04:00 -0600

    Inch closer to old commit

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -6,8 +6,8 @@ import chess.ChessPosition;
 import chess.InvalidMoveException;
 import chess.converter.ChessFunctions;
 import com.google.gson.Gson;
-import dataaccess.DataAccessException;
 import dataaccess.DatabaseService;
+import org.eclipse.jetty.server.Authentication;
 import websocket.ChessGameData;
 import websocket.commands.UserGameCommand;
 import io.javalin.websocket.WsContext;
@@ -16,34 +16,33 @@ import websocket.messages.ServerMessage;
 import java.util.HashMap;
 import java.util.Objects;
 
-
 public class WebSocketHandler {
 
-    private static final HashMap<String, connection> connections = new HashMap<>();
+    private static final HashMap<String, Connection> CONNECTIONS = new HashMap<>();
     private static final Gson GSON = new Gson();
 
-    private static class connection {
+    private static class Connection {
         public final WsContext client;
         public int gameId;
         public String authToken;
 
-        connection(WsContext client, int GameId) {
+        Connection(WsContext client, int gameId) {
             this.client = client;
-            this.gameId = GameId;
+            this.gameId = gameId;
         }
     }
 
     public static void addConnection(WsContext ctx) {
         ctx.enableAutomaticPings();
-        connections.put(ctx.sessionId(), new connection(ctx, 0));
+        CONNECTIONS.put(ctx.sessionId(), new Connection(ctx, 0));
     }
 
     public static void removeConnection(WsContext ctx) {
-        connections.remove(ctx.sessionId());
+        CONNECTIONS.remove(ctx.sessionId());
     }
 
     public static void handleWebSocketRequest(WsContext ctx, UserGameCommand command) {
-        if (!connections.containsKey(ctx.sessionId())) {
+        if (!CONNECTIONS.containsKey(ctx.sessionId())) {
             throw new AssertionError("Invalid Session ID");
         }
 
@@ -85,155 +84,22 @@ public class WebSocketHandler {
         switch (command.getCommandType()) {
             // Connect to a game
             case CONNECT -> {
-                // Store what game the session is in
-                connections.get(ctx.sessionId()).gameId = gameId;
-
-                // Store the user auth token
-                connections.get(ctx.sessionId()).authToken = command.getAuthToken();
-
-                // Send the game
-                sendMessage(
-                        ctx,
-                        new ServerMessage(
-                                ServerMessage.ServerMessageType.LOAD_GAME,
-                                game
-                        )
-                );
-
-                // broadcast to others that they have joined
-                String username = DatabaseService.getUsernameByAuthToken(command.getAuthToken());
-                ServerMessage alert = new ServerMessage(
-                        ServerMessage.ServerMessageType.NOTIFICATION,
-                        username + " has joined the game as " //+ userState
-                );
-                broadcastUniqueMessage(alert, connections.get(ctx.sessionId()).gameId, ctx);
+                handleConnect(ctx, command, gameId, game);
+                break;
             }
-
             // Execute a move
             case MAKE_MOVE -> {
-                // get the move
-                if (command.move == null) {
-                    throw new AssertionError("Error: Invalid Data");
-                }
-
-                // Assert game isn't finished
-                if (isGameOver(gameData)) {
-                    sendMessage(
-                            ctx,
-                            new ServerMessage(
-                                    ServerMessage.ServerMessageType.ERROR,
-                                    "Error: Game is already finished",
-                                    true
-                            )
-                    );
-                    return;
-                }
-
-                // Assert correct player is making the move
-                ChessGame.TeamColor activeTeam = gameData.game.getTeamTurn();
-                UserGameCommand.UserGameState expectedGameState = ChessFunctions.isWhite(activeTeam) ?
-                        UserGameCommand.UserGameState.WHITE : UserGameCommand.UserGameState.BLACK;
-
-                if (expectedGameState != userState) {
-                    sendMessage(
-                            ctx,
-                            new ServerMessage(
-                                    ServerMessage.ServerMessageType.ERROR,
-                                    "Error: Not your move...",
-                                    true
-                            )
-                    );
-                    return;
-                }
-
-                // Parse and execute the move
-                ChessMove move = new ChessMove(
-                        new ChessPosition(
-                                command.move.startPosition.row,
-                                command.move.startPosition.column
-                        ),
-                        new ChessPosition(
-                                command.move.endPosition.row,
-                                command.move.endPosition.column
-                        )
-                );
-
-                handleMoveRequest(command, ctx, game, move);
+                handleMakeMove(ctx, command, gameData, userState);
             }
 
             // leave a game
             case LEAVE -> {
-                // Remove the player from the database
-                String username = DatabaseService.getUsernameByAuthToken(command.getAuthToken());
-
-                Integer teamColorIndex = null;
-                if (userState == UserGameCommand.UserGameState.WHITE) {
-                    teamColorIndex = 0;
-                }
-                if (userState == UserGameCommand.UserGameState.BLACK) {
-                    teamColorIndex = 1;
-                }
-                if (teamColorIndex != null) {
-                    gameData.playerUsernames[teamColorIndex] = null;
-                    DatabaseService.updateGame(gameData);
-                }
-
-                broadcastUniqueMessage(
-                        new ServerMessage(
-                                ServerMessage.ServerMessageType.NOTIFICATION,
-                                username + " has left"
-                        ),
-                        connections.get(ctx.sessionId()).gameId,
-                        ctx
-                );
-
-                // Remove the session from active connections
-                connections.remove(ctx.sessionId());
+                handleLeave(ctx, command, gameData, userState);
             }
 
             // end the game
             case RESIGN -> {
-                if (isGameOver(gameData)) {
-                    sendMessage(
-                            ctx,
-                            new ServerMessage(
-                                    ServerMessage.ServerMessageType.ERROR,
-                                    "Error: game already over",
-                                    true
-                            )
-                    );
-                    return;
-                }
-
-                // Return the notification
-                String username = DatabaseService.getUsernameByAuthToken(command.getAuthToken());
-
-                Integer teamColorIndex = null;
-                if (Objects.equals(gameData.playerUsernames[0], username)) {
-                    teamColorIndex = 1;
-                }
-                if (Objects.equals(gameData.playerUsernames[1], username)) {
-                    teamColorIndex = 0;
-                }
-                if (teamColorIndex == null) {
-                    sendMessage(ctx, new ServerMessage(
-                            ServerMessage.ServerMessageType.ERROR,
-                            "Error: resignation not accepted due to user not existing",
-                            true
-                    ));
-                    return;
-                }
-
-                gameData.gameState = ChessGameData.gameStates.INACTIVE;
-                DatabaseService.updateGame(gameData);
-
-                broadcastMessage(
-                        new ServerMessage(
-                                ServerMessage.ServerMessageType.NOTIFICATION,
-                                gameData.playerUsernames[teamColorIndex] + " has won by resignation"
-                        ),
-                        connections.get(ctx.sessionId()).gameId
-                );
+                handleResign(ctx, command, gameData);
             }
         }
     }
@@ -243,22 +109,22 @@ public class WebSocketHandler {
         ctx.send(json);
     }
 
-    private static void broadcastMessage(ServerMessage message, int GameId) {
+    private static void broadcastMessage(ServerMessage message, int gameId) {
         String json = GSON.toJson(message);
         // Send the JSON to every connected client matching gameId
-        for (connection ctx : connections.values()) {
-            if (GameId == ctx.gameId) {
+        for (Connection ctx : CONNECTIONS.values()) {
+            if (gameId == ctx.gameId) {
                 ctx.client.send(json);
             }
         }
     }
 
-    private static void broadcastUniqueMessage(ServerMessage message, int GameId, WsContext sender) {
+    private static void broadcastUniqueMessage(ServerMessage message, int gameId, WsContext sender) {
         String json = GSON.toJson(message);
 
         // Send the JSON to every client matching gameId except the caller
-        for (connection ctx : connections.values()) {
-            if (GameId == ctx.gameId) {
+        for (Connection ctx : CONNECTIONS.values()) {
+            if (gameId == ctx.gameId) {
                 if (!ctx.client.sessionId().equals(sender.sessionId())) {
                     ctx.client.send(json);
                 }
@@ -296,7 +162,7 @@ public class WebSocketHandler {
                         ServerMessage.ServerMessageType.LOAD_GAME,
                         game
                 ),
-                connections.get(ctx.sessionId()).gameId
+                CONNECTIONS.get(ctx.sessionId()).gameId
         );
 
         broadcastUniqueMessage(
@@ -304,7 +170,7 @@ public class WebSocketHandler {
                         ServerMessage.ServerMessageType.NOTIFICATION,
                         "username moved"
                 ),
-                connections.get(ctx.sessionId()).gameId,
+                CONNECTIONS.get(ctx.sessionId()).gameId,
                 ctx
         );
 
@@ -316,9 +182,9 @@ public class WebSocketHandler {
                             ServerMessage.ServerMessageType.NOTIFICATION,
                             username + " has won by checkmate."
                     ),
-                    connections.get(ctx.sessionId()).gameId
+                    CONNECTIONS.get(ctx.sessionId()).gameId
             );
-            gameData.gameState = ChessGameData.gameStates.INACTIVE;
+            gameData.gameState = ChessGameData.GameStates.INACTIVE;
         }
 
         if (gameData.game.isInStalemate(gameData.game.getTeamTurn())) {
@@ -327,16 +193,16 @@ public class WebSocketHandler {
                             ServerMessage.ServerMessageType.NOTIFICATION,
                             "Draw by stalemate."
                     ),
-                    connections.get(ctx.sessionId()).gameId
+                    CONNECTIONS.get(ctx.sessionId()).gameId
             );
-            gameData.gameState = ChessGameData.gameStates.INACTIVE;
+            gameData.gameState = ChessGameData.GameStates.INACTIVE;
         }
 
         DatabaseService.updateGame(gameData);
     }
 
     private static boolean isGameOver(ChessGameData gameData) {
-        return gameData.gameState == ChessGameData.gameStates.INACTIVE;
+        return gameData.gameState == ChessGameData.GameStates.INACTIVE;
     }
 
     private static UserGameCommand.UserGameState getStateByAuthTokenAndGameId(String authToken, int gameId) {
@@ -353,5 +219,150 @@ public class WebSocketHandler {
         }
     }
 
-    ;
+    private static void handleConnect(WsContext ctx, UserGameCommand command, int gameId, ChessGame game) {
+            // Store what game the session is in
+            CONNECTIONS.get(ctx.sessionId()).gameId = gameId;
+
+            // Store the user auth token
+            CONNECTIONS.get(ctx.sessionId()).authToken = command.getAuthToken();
+
+            // Send the game
+            sendMessage(
+                    ctx,
+                    new ServerMessage(
+                            ServerMessage.ServerMessageType.LOAD_GAME,
+                            game
+                    )
+            );
+
+            // broadcast to others that they have joined
+            String username = DatabaseService.getUsernameByAuthToken(command.getAuthToken());
+            ServerMessage alert = new ServerMessage(
+                    ServerMessage.ServerMessageType.NOTIFICATION,
+                    username + " has joined the game as " //+ userState
+            );
+            broadcastUniqueMessage(alert, CONNECTIONS.get(ctx.sessionId()).gameId, ctx);
+        }
+
+    private static void handleMakeMove(WsContext ctx, UserGameCommand command, ChessGameData gameData, UserGameCommand.UserGameState userState) {
+        // get the move
+        if (command.move == null) {
+            throw new AssertionError("Error: Invalid Data");
+        }
+
+        // Assert game isn't finished
+        if (WebSocketHandler.isGameOver(gameData)) {
+            WebSocketHandler.sendMessage(
+                    ctx,
+                    new ServerMessage(
+                            ServerMessage.ServerMessageType.ERROR,
+                            "Error: Game is already finished",
+                            true
+                    )
+            );
+            return;
+        }
+
+        // Assert correct player is making the move
+        ChessGame.TeamColor activeTeam = gameData.game.getTeamTurn();
+        UserGameCommand.UserGameState expectedGameState = ChessFunctions.isWhite(activeTeam) ?
+                UserGameCommand.UserGameState.WHITE : UserGameCommand.UserGameState.BLACK;
+
+        if (expectedGameState != userState) {
+            WebSocketHandler.sendMessage(
+                    ctx,
+                    new ServerMessage(
+                            ServerMessage.ServerMessageType.ERROR,
+                            "Error: Not your move...",
+                            true
+                    )
+            );
+            return;
+        }
+
+        // Parse and execute the move
+        ChessMove move = new ChessMove(
+                new ChessPosition(
+                        command.move.startPosition.row,
+                        command.move.startPosition.column
+                ),
+                new ChessPosition(
+                        command.move.endPosition.row,
+                        command.move.endPosition.column
+                )
+        );
+    }
+
+    private static void handleLeave(WsContext ctx, UserGameCommand command, ChessGameData gameData, UserGameCommand.UserGameState userState) {
+        // Remove the player from the database
+        String username = DatabaseService.getUsernameByAuthToken(command.getAuthToken());
+
+        Integer teamColorIndex = null;
+        if (userState == UserGameCommand.UserGameState.WHITE) {
+            teamColorIndex = 0;
+        }
+        if (userState == UserGameCommand.UserGameState.BLACK) {
+            teamColorIndex = 1;
+        }
+        if (teamColorIndex != null) {
+            gameData.playerUsernames[teamColorIndex] = null;
+            DatabaseService.updateGame(gameData);
+        }
+
+        broadcastUniqueMessage(
+                new ServerMessage(
+                        ServerMessage.ServerMessageType.NOTIFICATION,
+                        username + " has left"
+                ),
+                CONNECTIONS.get(ctx.sessionId()).gameId,
+                ctx
+        );
+
+        // Remove the session from active connections
+        CONNECTIONS.remove(ctx.sessionId());
+    }
+
+    private static void handleResign(WsContext ctx, UserGameCommand command, ChessGameData gameData) {
+        if (isGameOver(gameData)) {
+            sendMessage(
+                    ctx,
+                    new ServerMessage(
+                            ServerMessage.ServerMessageType.ERROR,
+                            "Error: game already over",
+                            true
+                    )
+            );
+            return;
+        }
+
+        // Return the notification
+        String username = DatabaseService.getUsernameByAuthToken(command.getAuthToken());
+
+        Integer teamColorIndex = null;
+        if (Objects.equals(gameData.playerUsernames[0], username)) {
+            teamColorIndex = 1;
+        }
+        if (Objects.equals(gameData.playerUsernames[1], username)) {
+            teamColorIndex = 0;
+        }
+        if (teamColorIndex == null) {
+            sendMessage(ctx, new ServerMessage(
+                    ServerMessage.ServerMessageType.ERROR,
+                    "Error: resignation not accepted due to user not existing",
+                    true
+            ));
+            return;
+        }
+
+        gameData.gameState = ChessGameData.GameStates.INACTIVE;
+        DatabaseService.updateGame(gameData);
+
+        broadcastMessage(
+                new ServerMessage(
+                        ServerMessage.ServerMessageType.NOTIFICATION,
+                        gameData.playerUsernames[teamColorIndex] + " has won by resignation"
+                ),
+                CONNECTIONS.get(ctx.sessionId()).gameId
+        );
+    }
 }
